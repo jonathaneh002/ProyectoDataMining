4f10294586ba9d82ddc5cbcfd09689a66ac8c376
tujony <70181220+tujony@users.noreply.github.com>
2022-05-25 22:40:49 -0600

    Clusters

--- ProyectoUnsupervisedLearning.R
+++ ProyectoUnsupervisedLearning.R
@@ -4,6 +4,11 @@ library("dplyr")
 library('lubridate')
 library(readxl)
 library(countrycode)
+library(tidyr)
+library(purrr)
+library(dendextend)
+library(cluster)
+library(weights)
 
 ############################################
 #                                          #
@@ -14,8 +19,7 @@ library(countrycode)
 #                                          #
 #                                          #
 ############################################
-
-df<- read_excel("Online Retail.xlsx")
+df <- read_excel('Online Retail.xlsx')
 df$date<-as.Date(df$InvoiceDate)
 
 
@@ -162,7 +166,7 @@ df2%>%
 #-----------------------------------------------------------------
 
 #¿Las ventas presentan alguna estacionalidad por mes? 
-df2 <- mutate(df2, Month = month(df2$Date), Year = year(df2$Date))
+df2 <- mutate(df2, Month = month(df2$date), Year = year(df2$date))
 df2$Year <- as.factor(df2$Year)
 
 
@@ -258,6 +262,229 @@ df2%>%
 
 #f. Modelos:--------------------------------------------------------------------------------------------------------------
 #   a. Clustering (recomendación aplicarlo a clientes) 
+
+
+df_clustering <- df[df$Quantity > 0 & df$UnitPrice > 0, ]
+df_clustering <- drop_na(df_clustering, CustomerID)
+df_clustering <- drop_na(df_clustering, Country)
+
+
+df_clustering <- df_clustering %>% mutate(Total = (Quantity * UnitPrice) )
+
+df3 <- df_clustering %>% 
+  group_by(CustomerID) %>%
+  summarise(Total = sum(Total), Country = first(Country))
+
+
+frequency <- df_clustering %>% 
+                distinct(InvoiceNo, CustomerID) %>%
+                group_by(CustomerID) %>%
+                count()
+
+df3 <- cbind(df3, frequency[,2])
+df3 <- rename(df3, Frecuency = n)
+
+Last <- df_clustering %>% 
+          distinct(CustomerID, InvoiceDate)
+
+maximum <- (max(Last$InvoiceDate)) + days(1)
+Last$Last <- maximum - Last$InvoiceDate
+
+Last2 <- as.data.frame(Last %>% 
+  group_by(CustomerID) %>%
+  summarise(Last = min(Last)))
+
+df3 <- cbind(df3, Last2[,2])
+df3 <- rename(df3, Last = 'Last2[, 2]')
+
+
+Columnas = colnames(df3)
+Tipos = sapply(df3, class)
+
+nombres_DF = as.data.frame(cbind(Columnas, Tipos)) 
+
+variables_para_dummificar = nombres_DF%>%
+  filter(Tipos == "character")
+
+for(i in 1:nrow(variables_para_dummificar)) {
+  
+  columna_ref = as.character(variables_para_dummificar[i, 1])
+  temporal = select(df3, columna_ref)
+  colnames(temporal) = "REF"
+  temporal$REF = as.factor(temporal$REF)
+  temporal_dummy = dummify(temporal$REF)
+  nombres_dummy = colnames(temporal_dummy)
+  nombres_dummy = paste(columna_ref, nombres_dummy, sep = "-")
+  colnames(temporal_dummy) = nombres_dummy
+  archivo = if(i == 1){ archivo = temporal_dummy} else {archivo = cbind(archivo, temporal_dummy)}
+  
+}
+
+archivo = as.data.frame(archivo)
+
+
+#Datos atipicos
+
+
+#Total
+ggplot(df3, aes(x=Total)) +
+  geom_boxplot()
+
+
+Q1 = quantile(df3$Total, c(0.25))
+Q3 = quantile(df3$Total, c(0.75))
+RIQ = Q3 - Q1
+df4 = df3[(df3$Total >= (Q1 - 1.5*RIQ)) & (df3$Total <= (Q3 + 1.5*RIQ)), ]
+
+#Frecuency
+ggplot(df4, aes(x=Frecuency)) +
+  geom_boxplot()
+
+
+Q1 = quantile(df4$Frecuency, c(0.25))
+Q3 = quantile(df4$Frecuency, c(0.75))
+RIQ = Q3 - Q1
+df4 = df4[(df4$Frecuency >= (Q1 - 1.5*RIQ)) & (df4$Frecuency <= (Q3 + 1.5*RIQ)), ]
+
+
+#Last 
+ggplot(df4, aes(x=Last)) +
+  geom_boxplot()
+
+df4$Last <- as.numeric(df4$Last)
+
+Q1 = quantile(df4$Last, c(0.25))
+Q3 = quantile(df4$Last, c(0.75))
+RIQ = Q3 - Q1
+df4 = df4[(df4$Last >= (Q1 - 1.5*RIQ)) & (df4$Last <= (Q3 + 1.5*RIQ)), ]
+
+
+
+
+df_clustering <- (df4[c('Total','Frecuency','Last')])
+
+df_clustering <- scale(df_clustering)
+
+df_costumers <- df4
+
+#-------------------------Kmeans-----------------------------------------------#
+
+tot_withinss <- map_dbl(2:10,  function(k){
+  model <- kmeans(x = df_clustering, centers = k)
+  model$tot.withinss
+})
+
+# Genero un dataframe con el valor de k y de tot_withinss
+elbow_df <- data.frame(
+  k = 2:10 ,
+  tot_withinss = tot_withinss
+)
+
+# Muestro la gráfica de codo
+ggplot(elbow_df, aes(x = k, y = tot_withinss)) +
+  geom_line() +
+  scale_x_continuous(breaks = 2:10)
+
+
+# Uso map_dbl para correr varios modelos cambiando el valor de k
+sil_width <- map_dbl(2:10,  function(k){
+  model <- pam(x = df_clustering, k = k)
+  model$silinfo$avg.width
+})
+
+# Genero un dataframe que tenga el valor k y la silueta
+sil_df <- data.frame(
+  k = 2:10,
+  sil_width = sil_width
+)
+
+# Grafico la relación entre k y la silueta
+ggplot(sil_df, aes(x = k, y = sil_width)) +
+  geom_line() +
+  scale_x_continuous(breaks = 2:10)
+
+#Defino una semilla para que los valores sean comparables con la solución, debido a que k-means tiene un punto de partida aleatorio podría dar resultados diferentes y la única forma de asegurar que los resultados sean comparables es tener una semilla definida.
+
+set.seed(255)
+
+# Construir un modelo k-means model con los datos de customers_spend con k=4
+model_clientes <- kmeans(df_clustering, centers = 3)
+
+# Extraer el vactor con los valores de clusters asignados
+cluster_k <- as.data.frame(model_clientes$cluster)
+colnames(cluster_k) = "cluster_k"
+
+# Generar un dataframe con los datos de los clientes y el cluster
+segment_customers_k = bind_cols(df4[c('Total', 'Frecuency', 'Last')], cluster_k) %>%
+  mutate(cluster_k = as.factor(cluster_k))
+
+# Calculamos el tamaño de cada cluster
+Conteo_clusters1_char = segment_customers_k %>%
+  dplyr::group_by(cluster_k) %>%
+  dplyr::summarise(conteo = n())
+
+Conteo_clusters1_char
+
+# Graficamos el resultado de los 3 clusters
+clusplot(segment_customers_k, segment_customers_k$cluster_k, shape=TRUE, color=TRUE, labels=2, shade = T)
+
+
+# Calculo el promedio de cada categoría
+Resumen_K_char = segment_customers_k %>% 
+  group_by(cluster_k) %>% 
+  summarise_all(funs(mean(.)))
+
+Resumen_K_char
+
+#Comparación Total
+ggplot(segment_customers_k, aes(x=cluster_k, y=Total, fill=cluster_k)) + 
+  geom_boxplot()
+
+#Comparación Frecuency
+ggplot(segment_customers_k, aes(x=cluster_k, y=Frecuency, fill=cluster_k)) + 
+  geom_boxplot()
+
+#Comparación Last
+ggplot(segment_customers_k, aes(x=cluster_k, y=Last, fill=cluster_k)) + 
+  geom_boxplot()
+
+#------------------------------------------------------------------------------------------------------------#
+
+#---------------------------Jerarquico------------------------------------#
+
+dist_customers <- dist(df_clustering)
+
+# Realizo la clusterización según el método de vinculación que defina 
+hc_customers <- hclust(dist_customers, method = "complete")
+
+plot(hc_customers)
+
+cluster <- cutree(hc_customers, k=3)
+
+
+segment_customers = cbind(df_costumers, cluster)
+
+# Cuento la cantidad de clientes asignados a cada cluster
+count(segment_customers, cluster)
+
+# Agrego color al dendograma según el corte de altura definido
+dend_customers <- as.dendrogram(hc_customers)
+dend_colored <- color_branches(dend_customers, k = 3)
+
+# Grafico el dendograma con colores
+
+plot(dend_colored)
+rect.hclust(hc_customers, k=3) # agrega un rectángulo utilizando k
+
+
+# Calculo el promedio de cada categoría
+Resumen = segment_customers %>% 
+  group_by(cluster) %>% 
+  summarise_all(funs(mean(.)))
+
+Resumen
+
+#-------------------------------------------------------------------------------------------------------------#
 #   b. Association rules
 
 
